9d1e4cc610f99491346b82ff2f125c021c7279e9
Sara <saraahmad1114@yahoo.com>
2017-02-07 21:11:25 -0500

    created the xcode project

---
+++ Experimenting with NSCache/Experimenting with NSCacheTests/Experimenting_with_NSCacheTests.swift
@@ -0,0 +1,36 @@
+//
+//  Experimenting_with_NSCacheTests.swift
+//  Experimenting with NSCacheTests
+//
+//  Created by Flatiron School on 2/7/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import XCTest
+@testable import Experimenting_with_NSCache
+
+class Experimenting_with_NSCacheTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
